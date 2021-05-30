3be23ac31d6b52d0922357838bc3058390db1fa0
Jeroen van Straten <jeroen.van.straten@gmail.com>
2021-05-30 14:53:37 +0200

    Complete color variant selection

--- generator/orderlist.py
+++ generator/orderlist.py
@@ -1,5 +1,13 @@
 from part import get_part
 
+variants = ['O']
+
+def add(data, key, amount):
+    if key not in data:
+        data[key] = amount
+    else:
+        data[key] += amount
+
 data = {}
 
 for board in ('mainboard', 'support_board'):
@@ -9,27 +17,27 @@ for board in ('mainboard', 'support_board'):
             if not line:
                 continue
             part, *_ = line.split()
-            if part not in data:
-                data[part] = 1
-            else:
-                data[part] += 1
+            add(data, part, 1)
 
-mouser_order = []
-tinytronics_order = []
+mouser_order = {}
+tinytronics_order = {}
 
-for name, count in data.items():
-    data = get_part(name).get_data()
-    if 'mouser' in data:
-        mouser_order.append((data['mouser'], count))
-    elif 'tinytronics' in data:
-        tinytronics_order.append((data['tinytronics'], count))
-    else:
-        raise ValueError('no order info for part {}'.format(name))
+for variant in variants:
+    for name, count in data.items():
+        data = get_part(name).get_data()
+        if 'mouser' in data:
+            add(mouser_order, data['mouser'], count)
+        elif variant + '/mouser' in data:
+            add(mouser_order, data[variant + '/mouser'], count)
+        elif 'tinytronics' in data:
+            add(tinytronics_order, data['tinytronics'], count)
+        else:
+            raise ValueError('no order info for part {}'.format(name))
 
 with open('output/mouser.txt', 'w') as f:
-    for order_no, count in mouser_order:
+    for order_no, count in sorted(mouser_order.items()):
         f.write('{} {}\n'.format(order_no, count))
 
 with open('output/tinytronics.txt', 'w') as f:
-    for order_no, count in tinytronics_order:
+    for order_no, count in sorted(tinytronics_order.items()):
         f.write('{} {}\n'.format(order_no, count))
